d4b75035af3e09c9f0a92ab93a40a696f1bff471
Jan Prazak <janp@horasion.eu>
2023-02-02 11:21:56 +0100

    first rewrite without testing

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-# template-bootstrap-sass
-An optimised starter template for Bootstrap 5 and Sass (SCSS).
+# template_bootstrap5_sass_php
+An optimized starter template for Bootstrap 5.3 and Sass (SCSS), PHP Composer as dependency manager.
 
-Demo: https://amarok24.github.io/template-bootstrap-sass/
+Demo: https://amarok24.github.io/template_bootstrap5_sass_php/
 
 - Tested on different desktop browsers and on Google Android and Apple iOS devices
-- Optimised for accesibility (page readers)
+- Optimized for accesibility (page readers)
 - Accessible via keyboard: use "ESC" to open the navigation, "TAB" to switch between sections, "ENTER" for action
-- Optimised for fast loading
+- Optimized for fast loading
 - [PageSpeed Insights](https://developers.google.com/speed/pagespeed/insights/) results: mobile 94%, desktop 99%
 - Basic setup for switching between light/dark mode implemented
 
 ## Project use
 Click the "Use this template" button on the GitHub page to create your own copy directly in GitHub.
 
-Or clone via git. Be sure you have git and Node.js installed. Then run:
+Or clone via git. Be sure you have Git and Composer installed. Then run:
 
 ```sh
-git clone https://github.com/Amarok24/template-bootstrap-sass.git
+git clone https://github.com/Amarok24/template_bootstrap5_sass_php.git
 ```
 
 (An alternative way without using git is to download this repository as a ZIP file.)
 
 Then enter the created directory and install all dependencies (Bootstrap...) with:
 ```sh
-npm install
+composer install
 ```
--- assets/scripts/appendix.js
+++ assets/scripts/appendix.js
@@ -1,7 +1,7 @@
 /*
-  Appendix script for Bootstrap + Sass template.
-  Author: Jan Prazak
-  https://github.com/Amarok24/template-bootstrap-sass
+	Appendix script for Bootstrap + Sass template.
+	Author: Jan Prazak
+	https://github.com/Amarok24/template_bootstrap5_sass_php
 */
 
 // Edit the YouTube video ID here! Video will be loaded on demand (page performance).
@@ -19,8 +19,8 @@ const elemScrollToTop = document.querySelector('#arrowScrollToTop');
 const elemToggleLightDarkMode = document.querySelector('#toggleLightDarkMode');
 
 const intersectApiSupported = (
-  'IntersectionObserver' in window &&
-  'IntersectionObserverEntry' in window);
+	'IntersectionObserver' in window &&
+	'IntersectionObserverEntry' in window);
 
 let bsPageNav = null;
 let bsVideoModal = null;
@@ -29,166 +29,178 @@ let youtubeApiInjected = false;
 
 
 if (elemVideoModal && elemPlayButton) {
-  //bsVideoModal = bootstrap.Modal.getOrCreateInstance(elemVideoModal);
-  bsVideoModal = new bootstrap.Modal(elemVideoModal);
+	//bsVideoModal = bootstrap.Modal.getOrCreateInstance(elemVideoModal);
+	bsVideoModal = new bootstrap.Modal(elemVideoModal);
 
-  elemVideoModal.addEventListener('hidden.bs.modal', function (ev) {
-    console.log('bsVideoModal has been hidden');
-    if (youtubePlayer) youtubePlayer.pauseVideo();
-  });
+	elemVideoModal.addEventListener('hidden.bs.modal', function (ev)
+	{
+		console.log('bsVideoModal has been hidden');
+		if (youtubePlayer) youtubePlayer.pauseVideo();
+	});
 
-  function injectYoutubeApi(ev) {
-    if (ev.type === 'keyup' && ev.key !== 'Enter') return;
-    console.log('showing bsVideoModal');
-    bsVideoModal.show();
+	function injectYoutubeApi(ev)
+	{
+		if (ev.type === 'keyup' && ev.key !== 'Enter') return;
+		console.log('showing bsVideoModal');
+		bsVideoModal.show();
 
-    if (youtubeApiInjected) {
-      console.log('playing video...');
-      youtubePlayer.playVideo();
-      return;
-    }
-    const tagScript = document.createElement('script');
-    tagScript.src = 'https://www.youtube.com/iframe_api';
-    let firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tagScript, firstScriptTag);
-    console.log("YouTube API code injected");
-    youtubeApiInjected = true;
-  }
+		if (youtubeApiInjected) {
+			console.log('playing video...');
+			youtubePlayer.playVideo();
+			return;
+		}
+		const tagScript = document.createElement('script');
+		tagScript.src = 'https://www.youtube.com/iframe_api';
+		let firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tagScript, firstScriptTag);
+		console.log("YouTube API code injected");
+		youtubeApiInjected = true;
+	}
 
-  elemPlayButton.addEventListener('click', injectYoutubeApi);
-  elemPlayButton.addEventListener('keyup', injectYoutubeApi);
+	elemPlayButton.addEventListener('click', injectYoutubeApi);
+	elemPlayButton.addEventListener('keyup', injectYoutubeApi);
 }
 
 
 if (elemNavbarCollapsible && elemNavAnchors) {
-  //bsPageNav = new bootstrap.Collapse(elemNavbarCollapsible, {toggle: false});
-  // 'toggle:false' means it should not be toggled (opened) upon creation.
-  bsPageNav = bootstrap.Collapse.getOrCreateInstance(elemNavbarCollapsible, { toggle: false });
+	//bsPageNav = new bootstrap.Collapse(elemNavbarCollapsible, {toggle: false});
+	// 'toggle:false' means it should not be toggled (opened) upon creation.
+	bsPageNav = bootstrap.Collapse.getOrCreateInstance(elemNavbarCollapsible, { toggle: false });
 
-  function toggleHamburger() {
-    if (bsPageNav) bsPageNav.toggle();
-    elemNavAnchors[0].focus();
-  }
+	function toggleHamburger()
+	{
+		if (bsPageNav) bsPageNav.toggle();
+		elemNavAnchors[0].focus();
+	}
 
-  function closeHamburger() {
-    if (bsPageNav) bsPageNav.hide();
-  }
+	function closeHamburger()
+	{
+		if (bsPageNav) bsPageNav.hide();
+	}
 
-  for (let i = 0; i < elemNavAnchors.length; i++) {
-    elemNavAnchors[i].addEventListener('click', closeHamburger);
-  }
+	for (let i = 0; i < elemNavAnchors.length; i++) {
+		elemNavAnchors[i].addEventListener('click', closeHamburger);
+	}
 
-  document.addEventListener('keyup', function (ev) {
-    if (ev.key === 'Escape' || ev.key === 'Esc') toggleHamburger();
-  });
+	document.addEventListener('keyup', function (ev)
+	{
+		if (ev.key === 'Escape' || ev.key === 'Esc') toggleHamburger();
+	});
 }
 
 
 if (elemScrollToTop) {
-  function onScrollToTopClick(ev) {
-    ev.stopPropagation();
-    ev.preventDefault();
-    document.querySelector('body').scrollIntoView({ block: 'start', behavior: 'smooth' });
-  }
+	function onScrollToTopClick(ev)
+	{
+		ev.stopPropagation();
+		ev.preventDefault();
+		document.querySelector('body').scrollIntoView({ block: 'start', behavior: 'smooth' });
+	}
 
-  elemScrollToTop.addEventListener('click', onScrollToTopClick);
+	elemScrollToTop.addEventListener('click', onScrollToTopClick);
 }
 
 
-function launchObserver() {
-  const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.3
-  };
+function launchObserver()
+{
+	const options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.3
+	};
 
-  const pageNav = document.querySelector('#pageNav');
-  const customCssClass = navBarCustomCssClass;
+	const pageNav = document.querySelector('#pageNav');
+	const customCssClass = navBarCustomCssClass;
 
-  let observer = new IntersectionObserver(handleIntersect, options);
-  let elem = document.querySelector('.header-image');
+	let observer = new IntersectionObserver(handleIntersect, options);
+	let elem = document.querySelector('.header-image');
 
-  observer.observe(elem);
+	observer.observe(elem);
 
-  function handleIntersect(entries, observer) {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-    if (pageNav) {
-      if (entries[0].isIntersecting) {
-        pageNav.classList.remove(customCssClass);
-        elemScrollToTop?.classList.add('d-none');
-      } else {
-        pageNav.classList.add(customCssClass);
-        elemScrollToTop?.classList.remove('d-none');
-      }
-    }
-  }
+	function handleIntersect(entries, observer)
+	{
+		// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+		if (pageNav) {
+			if (entries[0].isIntersecting) {
+				pageNav.classList.remove(customCssClass);
+				elemScrollToTop?.classList.add('d-none');
+			} else {
+				pageNav.classList.add(customCssClass);
+				elemScrollToTop?.classList.remove('d-none');
+			}
+		}
+	}
 }
 
 
 // Part of YT API. This event fires automatically once API is ready.
-function onYouTubeIframeAPIReady() {
-  console.log('onYouTubeIframeAPIReady launched');
-  youtubePlayer = new YT.Player(videoCssId, {
-    width: '560',
-    height: '315',
-    videoId: youtubeVideoId,
-    playerVars: {
-      // See https://developers.google.com/youtube/player_parameters
-      'playsinline': 1,
-      'rel': 0,
-      'modestbranding': 1
-    },
-    events: {
-      'onReady': onPlayerReady
-      //'onStateChange': onPlayerStateChange
-    }
-  });
+function onYouTubeIframeAPIReady()
+{
+	console.log('onYouTubeIframeAPIReady launched');
+	youtubePlayer = new YT.Player(videoCssId, {
+		width: '560',
+		height: '315',
+		videoId: youtubeVideoId,
+		playerVars: {
+			// See https://developers.google.com/youtube/player_parameters
+			'playsinline': 1,
+			'rel': 0,
+			'modestbranding': 1
+		},
+		events: {
+			'onReady': onPlayerReady
+			//'onStateChange': onPlayerStateChange
+		}
+	});
 }
 
 // This event fires when player has finished loading and is ready.
-function onPlayerReady(ev) {
-  //const embedCode = ev.target.getVideoEmbedCode();
-  //ev.target.playVideo();
-  console.log('onPlayerReady launched');
-  youtubePlayer.playVideo();
+function onPlayerReady(ev)
+{
+	//const embedCode = ev.target.getVideoEmbedCode();
+	//ev.target.playVideo();
+	console.log('onPlayerReady launched');
+	youtubePlayer.playVideo();
 }
 
 
-function handleVisibilityChange() {
-  if (youtubePlayer && document.visibilityState === 'hidden') {
-    youtubePlayer.pauseVideo();
-  }
+function handleVisibilityChange()
+{
+	if (youtubePlayer && document.visibilityState === 'hidden') {
+		youtubePlayer.pauseVideo();
+	}
 }
 
 
-function toggleLightDarkMode(ev) {
-  ev.preventDefault();
-  ev.stopPropagation();
+function toggleLightDarkMode(ev)
+{
+	ev.preventDefault();
+	ev.stopPropagation();
 
-  let isDarkMode = false;
-  const modeDataNow = document.body.dataset.darkmode;
-  if (!modeDataNow) return;
+	let isDarkMode = false;
+	const modeDataNow = document.body.dataset.darkmode;
+	if (!modeDataNow) return;
 
-  isDarkMode = modeDataNow === "0" ? false : true;
+	isDarkMode = modeDataNow === "0" ? false : true;
 
-  if (isDarkMode) {
-    console.log('Switching to light mode');
-    document.body.dataset.darkmode = '0';
-    elemPageNav.classList.remove('navbar-dark');
-    elemPageNav.classList.add('navbar-light');
-  } else {
-    console.log('Switching to dark mode');
-    document.body.dataset.darkmode = '1';
-    elemPageNav.classList.remove('navbar-light');
-    elemPageNav.classList.add('navbar-dark');
-  }
+	if (isDarkMode) {
+		console.log('Switching to light mode');
+		document.body.dataset.darkmode = '0';
+		elemPageNav.classList.remove('navbar-dark');
+		elemPageNav.classList.add('navbar-light');
+	} else {
+		console.log('Switching to dark mode');
+		document.body.dataset.darkmode = '1';
+		elemPageNav.classList.remove('navbar-light');
+		elemPageNav.classList.add('navbar-dark');
+	}
 }
 
 
 if (intersectApiSupported) launchObserver();
 
 if (elemToggleLightDarkMode && elemPageNav) {
-  elemToggleLightDarkMode.addEventListener('click', toggleLightDarkMode);
+	elemToggleLightDarkMode.addEventListener('click', toggleLightDarkMode);
 }
 
 document.addEventListener('visibilitychange', handleVisibilityChange, false);
