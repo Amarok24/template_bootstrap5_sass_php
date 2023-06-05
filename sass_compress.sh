1db1e507444744b754102f322c12a7567fcc6fd0
Jan Prazak <janp@horasion.eu>
2023-06-05 18:22:15 +0200

    fixed incompatible shebang in scripts

--- sass_compress.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Make sure you have `sass` executable in your path.
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	sass --style=compressed scss:assets/css
-else
-	sass.bat --style=compressed scss:assets/css
-fi
